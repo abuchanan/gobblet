3e1d945722812d576f1e4218eab628612840c7b0
Alex Buchanan <buchanae@gmail.com>
2014-03-01 11:39:49 -0800

    fix dugout tests

--- tests/test_dugout.py
+++ tests/test_dugout.py
@@ -12,75 +12,67 @@ class DugoutTestCase(unittest.TestCase):
         num_stacks = 2
         self.dugout = gobblet.Dugout(player, sizes, num_stacks)
 
+    def assertPieces(self, pieces, expected):
+        for piece, expect in zip(pieces, expected):
+            player, size = expect
+            self.assertEqual(piece.player, player)
+            self.assertEqual(piece.size, size)
+
     def test_init(self):
         expected_stack = [
-            gobblet.Piece('sally', gobblet.Sizes.xs),
-            gobblet.Piece('sally', gobblet.Sizes.sm),
-            gobblet.Piece('sally', gobblet.Sizes.lg),
-            gobblet.Piece('sally', gobblet.Sizes.xl),
+            ('sally', gobblet.Sizes.xs),
+            ('sally', gobblet.Sizes.sm),
+            ('sally', gobblet.Sizes.lg),
+            ('sally', gobblet.Sizes.xl),
         ]
-        expected_stacks = [expected_stack] * 2
-        self.assertEqual(self.dugout.stacks, expected_stacks)
+        self.assertEqual(len(self.dugout.stacks), 2)
+        self.assertPieces(self.dugout.stacks[0], expected_stack)
+        self.assertPieces(self.dugout.stacks[1], expected_stack)
 
     def test_available_pieces(self):
         expected_pieces = [
-            gobblet.Piece('sally', gobblet.Sizes.xl),
-            gobblet.Piece('sally', gobblet.Sizes.xl),
+            ('sally', gobblet.Sizes.xl),
+            ('sally', gobblet.Sizes.xl),
         ]
-        self.assertEqual(self.dugout.available_pieces(), expected_pieces)
+        self.assertPieces(self.dugout.available_pieces(), expected_pieces)
 
         self.dugout.stacks[0].pop()
 
         expected_pieces = [
-            gobblet.Piece('sally', gobblet.Sizes.lg),
-            gobblet.Piece('sally', gobblet.Sizes.xl),
+            ('sally', gobblet.Sizes.lg),
+            ('sally', gobblet.Sizes.xl),
         ]
-        self.assertEqual(self.dugout.available_pieces(), expected_pieces)
+        self.assertPieces(self.dugout.available_pieces(), expected_pieces)
 
     def test_use_piece(self):
-        piece = gobblet.Piece('sally', gobblet.Sizes.xl)
+        piece = self.dugout.available_pieces()[0]
         self.dugout.use_piece(piece)
 
         expected_pieces = [
-            gobblet.Piece('sally', gobblet.Sizes.lg),
-            gobblet.Piece('sally', gobblet.Sizes.xl),
+            ('sally', gobblet.Sizes.lg),
+            ('sally', gobblet.Sizes.xl),
         ]
-        self.assertEqual(self.dugout.available_pieces(), expected_pieces)
+        self.assertPieces(self.dugout.available_pieces(), expected_pieces)
 
-        piece = gobblet.Piece('sally', gobblet.Sizes.xl)
+        piece = self.dugout.available_pieces()[1]
         self.dugout.use_piece(piece)
 
         expected_pieces = [
-            gobblet.Piece('sally', gobblet.Sizes.lg),
-            gobblet.Piece('sally', gobblet.Sizes.lg),
+            ('sally', gobblet.Sizes.lg),
+            ('sally', gobblet.Sizes.lg),
         ]
-        self.assertEqual(self.dugout.available_pieces(), expected_pieces)
+        self.assertPieces(self.dugout.available_pieces(), expected_pieces)
 
         self.dugout.stacks[0] = []
 
         expected_pieces = [
-            gobblet.Piece('sally', gobblet.Sizes.lg),
+            ('sally', gobblet.Sizes.lg),
         ]
-        self.assertEqual(self.dugout.available_pieces(), expected_pieces)
-
-    def test_use_piece_copy(self):
-        # The Player class has a DugoutAPI that copies the pieces to avoid
-        # public modification of internal game state, so it's important
-        # that the Player class can call Dugout.use_piece() with a copy
-        # of the original piece.
-        piece = self.dugout.available_pieces()[0]
-        piece_copy = deepcopy(piece)
-
-        self.dugout.use_piece(piece_copy)
-
-        expected_pieces = [
-            gobblet.Piece('sally', gobblet.Sizes.lg),
-            gobblet.Piece('sally', gobblet.Sizes.xl),
-        ]
-        self.assertEqual(self.dugout.available_pieces(), expected_pieces)
+        self.assertPieces(self.dugout.available_pieces(), expected_pieces)
 
     def test_NoSuchPiece(self):
-        piece = gobblet.Piece('sally', gobblet.Sizes.xs)
+        # Get a piece from the bottom of a stack
+        piece = self.dugout.stacks[0][0]
 
         with self.assertRaises(self.dugout.NoSuchPiece):
             self.dugout.use_piece(piece)
